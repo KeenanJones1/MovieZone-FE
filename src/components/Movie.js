e0335d66d173c3e2c0a1c8e8940d0a8a53c18373
Keenan Jones <keenanjones210@gmail.com>
2021-06-05 18:16:48 -0500

    Working

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -100,7 +100,7 @@ const Movie = ({movie, updateMovie}) => {
    data: {uuid: uuid, query: movie.id, thumbs: name, title: movie.title}
   }
   axios.request(options).then((resp) =>{
-    await updateMovie(resp.data.movie)
+    updateMovie(resp.data.movie)
   }).catch((err) => console.log(err))
  }
 
